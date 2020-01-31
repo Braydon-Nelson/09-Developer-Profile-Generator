90ee3313f69ea58f00bf507f484ea7335077f3db
Braydon Nelson <braydon.nelson@outlook.com>
2020-01-30 23:27:41 -0700

    added comments

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//Beginning varibles..........
 const fs = require("fs");
 const generateHTML = require("./generateHTML");
 const inquirer = require("inquirer");
 const axios = require("axios");
 var pdf = require('html-pdf');
 
-//Questions array used to prompt user.........
+//Questions array used to prompt user
 const questions = [
     {
         type: "input",
@@ -24,20 +23,18 @@ const questions = [
         ]
     }
 ];
-//Prompt user with questions array using inquirer, then based on user name query GitHub for that users data...
+//Prompt user with questions, then based on user name axios call GitHub for that users data
 inquirer.prompt(questions).then(function ({ username, color }) {
     console.log(username, color);
     const queryUrl = `https://api.github.com/users/${username}`;
 
     axios.get(queryUrl).then(function (response) {
         console.log(response.data)
-
-        /* IF we wanted to stay in the browser we would write to file. */
+        //Code for Just HTML Page
         fs.writeFile("./generate.html", generateHTML(response, color), function (err, result) {
             if (err) console.log('error', err);
         })
-
-        //  Generating pdf and calling our generateHTML function pushing our axious response into the html file.....
+        //Code for html-pdf
         pdf.create(generateHTML(response, color)).toFile('./profile.pdf', function (err, res) {
             if (err) return console.log(err);
             console.log(res);
